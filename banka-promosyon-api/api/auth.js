61391130e3834dcff6b385af1f22b89c691f84e0
Ersin Göç <ersin_1229@hotmail.com>
2025-04-04 13:37:27 +0300

    cors 5. düzeltme

--- banka-promosyon-api/api/auth.js
+++ banka-promosyon-api/api/auth.js
@@ -2,14 +2,13 @@
 
 import { createClient } from "@supabase/supabase-js";
 import jwt from "jsonwebtoken";
-import { allowCors } from "./_cors";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-async function handler(req, res) {
+export default async function handler(req, res) {
   // --- CORS BAŞLANGIÇ ---
   res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -121,5 +120,3 @@ async function handler(req, res) {
 
   return res.status(405).json({ error: "Method not allowed" });
 }
-
-export default allowCors(handler)
